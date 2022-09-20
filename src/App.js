24ad1eb2fec40e36b268dabfaffa235fa07ff250
RemXYZ <remkarablin20@gmail.com>
2022-09-20 23:28:19 +0200

    Menu component, fonts, backgound

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import 'styles/App.css'
+import 'styles/Fonts.css'
+import { BrowserRouter, Route, Routes, Link, Navigate } from 'react-router-dom';
+import Menu from "UI/Menu/Menu.jsx"
 
 function App() {
 return (
+<BrowserRouter>
     <div>
-        <h1>Hello World!</h1>
+        <Menu></Menu>
     </div>
+</BrowserRouter>
 )
 }
 
---
+++ src/UI/Menu/Menu.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Menu.css'
+import { BrowserRouter, Route, Routes, Link, Navigate } from 'react-router-dom';
+
+// RS - imr
+//RS - sfc Stateless Function Component
+const Menu = () => {
+    return ( 
+        <div className='menu_background'>
+            <div className="menu">
+                <Link to="/home" className="logo">Azixon</Link>
+                <nav>
+                    <Link to="#" className="nav_item">ABOUT</Link>
+                    <Link to="#" className="nav_item">BLOG</Link>
+                    <Link to="#" className="nav_item">CONTACT</Link>
+                    <Link to="#" className="nav_item">#</Link>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+export default Menu;
