1b67d59a7dafae562f48bad17fb386611a57221e
RemXYZ <remkarablin20@gmail.com>
2023-01-04 23:02:49 +0100

    Content0

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import 'styles/App.css'
 import 'styles/Fonts.css'
 import { BrowserRouter, Route, Routes, Link, Navigate } from 'react-router-dom';
 import Menu from "UI/Menu/Menu.jsx"
+import { useState } from 'react';
+
 
 function App() {
+const [likes, setLikes] = useState(5);
+function increment() {
+    
+    console.log(likes)
+    setLikes(likes + 1)
+}
+    
 return (
+<div className='Root'>
 <BrowserRouter>
     <div>
         <Menu></Menu>
     </div>
 </BrowserRouter>
+<div>{likes}</div>
+<button onClick={ ()=> increment() }>Like</button>
+</div>
 )
 }
 
